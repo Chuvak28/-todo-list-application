cf6dc899ccb7e1ace2ad067486e171939d3f2362
Chuvak28 <yuldashevj@gmail.com>
2022-05-07 11:23:18 +0500

    created Interfaces

---
+++ ToDo.BLL/Interfaces/ITodoEntryDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.BLL.Entity;
+
+namespace ToDo.BLL.Interfaces
+{
+    public interface ITodoEntryDataProvider
+    {
+        public string Create(TODOEntry item);
+        public string SetStatus(int numberList, string statusChanged);
+        public string Remove(int id);
+        public TODOEntry Update(TODOEntry item);
+        public List<TODOEntry> GetAll();
+        public TODOEntry Get(int id);
+    }
+}
