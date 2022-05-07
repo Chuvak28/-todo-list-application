53fa7686ff71602d92698db9c621d50c3849bf43
Chuvak28 <yuldashevj@gmail.com>
2022-05-07 15:45:46 +0500

    Modified readme

---
+++ ToDo.BLL/Operations/ToDoEntryOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.BLL.Entity;
+using ToDo.BLL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace ToDo.BLL.Operations
+{
+    public class ToDoEntryOperations : ITodoEntryDataProvider
+    {
+        public string Create(TODOEntry item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TODOEntry Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TODOEntry> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetStatus(int id, string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TODOEntry Update(TODOEntry item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ToDo.BLL/Operations/ToDoListOperations.cs
+++ ToDo.BLL/Operations/ToDoListOperations.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using ToDo.BLL.Entity;
 using ToDo.BLL.Interfaces;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 
